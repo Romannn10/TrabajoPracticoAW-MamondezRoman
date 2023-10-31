3860fa91941272e9f1fa5bebe7386f8b5f84dbb7
root <mgiraudo@ies21.edu.ar>
2023-10-31 10:47:43 -0300

    cambio

---
+++ TP-AW-MamondezRoman-main/jugadores.js
@@ -0,0 +1,19 @@
+const jugadores = {
+    nombre:"Jugadores",
+    jugadores: [
+                 {
+                 nombre:"a",
+                 edad:"",
+                 imagen:"./img/Romero.webp",
+                 nacionalidad:"",
+                 posicion:"",
+                 dorsal:""
+                }
+
+                ]
+
+}
+
+
+
+export default jugadores
