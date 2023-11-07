9477dc693feb28666f6da4e738d1d226f2611163
root <mgiraudo@ies21.edu.ar>
2023-11-07 10:21:36 -0300

    cambio

--- TP-AW-MamondezRoman-main/jugadores.js
+++ TP-AW-MamondezRoman-main/jugadores.js
@@ -2,14 +2,53 @@ const jugadores = {
     nombre:"Jugadores",
     jugadores: [
                  {
-                 nombre:"a",
-                 edad:"",
+                 nombre:"Sergio Romero",
+                 edad:"36",
                  imagen:"./img/Romero.webp",
-                 nacionalidad:"",
-                 posicion:"",
-                 dorsal:""
-                }
-
+                 nacionalidad:"Argentina",
+                 posicion:"Portero",
+                 dorsal:"1"
+                },
+                {
+                 nombre:"Luis Advincula",
+                 edad:"33",
+                 imagen:"./img/advincula.webp",
+                 nacionalidad:"Peru",
+                 posicion:"Lateral Derecho",
+                 dorsal:"17"
+                },
+                {
+                 nombre:"Valentin Barco",
+                 edad:"19",
+                 imagen:"./img/23_120.png",
+                 nacionalidad:"Argentina",
+                 posicion:"Lateral Izquierdo",
+                 dorsal:"19"
+                },
+                {
+                 nombre:"Marcos Rojo",
+                 edad:"33",
+                 imagen:"./img/rojo.webp",
+                 nacionalidad:"Argentina",
+                 posicion:"Defensa Central",
+                 dorsal:"6"
+                },
+                {
+                 nombre:"Nicolas Figal",
+                 edad:"29",
+                 imagen:"./img/figal.webp",
+                 nacionalidad:"Argentina",
+                 posicion:"Defensa Central",
+                 dorsal:"4"
+                },
+                {
+                 nombre:"Pol Fernandez",
+                 edad:"31",
+                 imagen:"./img/pol.webp",
+                 nacionalidad:"Argentina",
+                 posicion:"Medio Centro",
+                 dorsal:"8"
+                },
                 ]
 
 }
